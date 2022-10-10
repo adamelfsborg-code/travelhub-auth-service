abf2427149170f80d8fcaf43faaaa3b71741e77d
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2022-10-10 20:03:17 +0200

    add user database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { getEnvPath } from './common/helper/env.helper';
 
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ConfigModule.forRoot({ envFilePath, isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth/auth.service';
 import { AuthController } from './controllers/auth/auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'travel_hub',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AuthModule {}
